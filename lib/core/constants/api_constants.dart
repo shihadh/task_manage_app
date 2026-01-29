8f931356ad53733435a954b24238c66ffc720618
shihadh <majesticzad786@gmail.com>
2026-01-29 12:56:31 +0530

    remove  backend

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://10.0.2.2:3000'; // For Android Emulator
-  // Use http://localhost:3000 for iOS Simulator or Web
+  static const String baseUrl = 'http://10.0.2.2:4000';
 
   static const String login = '/login';
   static const String tasks = '/tasks';
